191f8a3e49948a3ffbcb5fd4b629999ab43f3f34
tomhine <tomhine792@gmail.com>
2022-02-11 22:00:19 +0000

    Design add idea form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,9 +54,52 @@
 
     <main class="container max-w-custom mx-auto flex gap-5">
         <div class="w-70">
-            Add idea form goes here. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti quos vitae
-            debitis velit rerum sit laborum itaque molestias sequi, modi, id tenetur quod ea tempora quas explicabo sed
-            dignissimos. Maxime delectus excepturi perspiciatis aliquid id iure. Exercitationem iusto officia expedita!
+            <div class="p-0.5 mt-16 rounded-xl bg-gradient-to-t from-transparent via-blue/20 to-blue/30">
+                <div class="bg-white rounded-xl">
+                    <div class="text-center px-4 py-2 pt-6">
+                        <h3 class="font-semibold text-base">Add an idea</h3>
+                        <p class="text-xs mt-4">Let us know what you would like and we'll take a look over!</p>
+
+                        <div>
+                            <form action="#" method="POST" class="space-y-4 py-6">
+                                <div>
+                                    <input type="text"
+                                        class="w-full text-sm bg-gray-100 border-none rounded-xl placeholder:text-gray-900 px-4 py-2"
+                                        placeholder="Your idea">
+                                </div>
+                                <div>
+                                    <select name="category_add" id="category_add"
+                                        class="w-full bg-gray-100 text-sm rounded-xl border-none px-4 py-2">
+                                        <option value="Category One">Category One</option>
+                                        <option value="Category Two">Category Two</option>
+                                        <option value="Category Three">Category Three</option>
+                                        <option value="Category Four">Category Four</option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <textarea name="idea" id="idea" cols="30" rows="4"
+                                        class="w-full bg-gray-100 border-none rounded-xl resize-none placeholder:text-gray-900 text-sm px-4 py-2"
+                                        placeholder="Describe your idea"></textarea>
+                                </div>
+                                <div class="flex items-center justify-between gap-3">
+                                    <button
+                                        class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition ease-in px-6 py-3"
+                                        type="submit">
+                                        <x-icon.paperclip />
+                                        <span class="ml-1">Attach</span>
+                                    </button>
+                                    <button
+                                        class="flex items-center justify-center w-1/2 h-11 text-xs text-white bg-blue font-semibold rounded-xl border border-blue hover:bg-blue-hover transition ease-in px-6 py-3"
+                                        type="button">
+                                        Submit
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="w-175">
             <nav class="flex items-center justify-between text-xs">

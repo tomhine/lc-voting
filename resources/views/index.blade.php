f85db8aa878e247b48bb524ca777d84ef37477cc
tomhine <tomhine792@gmail.com>
2022-02-11 21:10:49 +0000

    Design idea cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,6 +24,73 @@
         <x-icon.search />
       </div>
     </div>
-  </div>
+  </div> <!-- end filters -->
+
+  <div class="ideas-container space-y-6 my-6">
+    <div class="idea-container bg-white rounded-xl flex transition ease-in cursor-pointer hover:shadow-card">
+      <div class="border-r border-gray-100 px-5 py-8">
+        <div class="text-center">
+          <div class="font-semibold text-2xl">12</div>
+          <div class="text-gray-500">Votes</div>
+        </div>
+        <div class="mt-8">
+          <button class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl px-4 py-3 transition ease-in
+              hover:border-gray-400">Vote</button>
+        </div>
+      </div>
+      <div class="flex px-2 py-6">
+        <a href="#" class="flex-none">
+          <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl">
+        </a>
+        <div class="mx-4">
+          <h4 class="text-xl font-semibold ">
+            <a href="#" class="hover:underline">A random title can go here</a>
+          </h4>
+          <div class="text-gray-600 mt-3 line-clamp-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+            alias sapiente
+            dolor illo est voluptatum repellat laboriosam quos in ea odit, quidem cum consequuntur ipsam nulla pariatur
+            architecto aperiam quibusdam consequatur aliquid maiores ipsa ullam molestiae tempora? Eum suscipit vel
+            officiis molestias. Sed eligendi debitis temporibus quibusdam quasi numquam enim delectus repellendus
+            deleniti sit! Laborum quaerat voluptates aliquid blanditiis possimus ullam rem vel non obcaecati distinctio
+            a incidunt, quae corporis minima aliquam libero mollitia id nemo architecto, voluptate at dolore modi. Neque
+            magnam iste dolor, fugit officiis amet exercitationem provident voluptatibus ipsa soluta est reiciendis,
+            voluptatum, quae quod facilis error!
+          </div>
+          <div class="flex items-center justify-between mt-6">
+            <div class="flex items-center text-xs font-semibold space-x-2 text-gray-400">
+              <time>10 hours ago</time>
+              <div>&bull;</div>
+              <div>Category</div>
+              <div>&bull;</div>
+              <div class="text-gray-800">3 Comments</div>
+            </div>
+            <div class="flex items-center space-x-2">
+              <div
+                class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
+                Open
+              </div>
+              <div>
+                <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transitin ease-in py-2 px-3">
+                  <x-icon.ellipsis />
+                </button>
+                <ul class="absolute w-44 ml-8 font-semibold bg-white shadow-dialog rounded-xl py-3">
+                  <li>
+                    <a href="#" class="block hover:bg-gray-100 px-5 py-3 transition ease-in">
+                      Mark as Spam
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" class="block hover:bg-gray-100 px-5 py-3 transition ease-in">
+                      Delete
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> <!-- end idea container -->
+  </div> <!-- end ideas container -->
 
 </x-app-layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,8 +28,18 @@ module.exports = {
             fontFamily: {
                 sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
             },
+            fontSize: {
+                xxs: ["0.625rem", { lineHeight: "1rem" }],
+            },
+            boxShadow: {
+                card: "4px 4px 15px 0 rgba(36, 37, 38, 0.08)",
+                dialog: "3px 4px 15px 0 rgba(36, 37, 38, 0.22)",
+            },
         },
     },
 
-    plugins: [require("@tailwindcss/forms")],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/line-clamp"),
+    ],
 };
